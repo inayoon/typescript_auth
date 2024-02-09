24905ad61bc5d1ae62187fd5aa224e66c1fed61c
Ina Yoon <yia0319@gmail.com>
2024-02-09 12:29:12 -0500

    Created auth route & controller

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import router from './router'
 
 const app = express();
 app.use(cors({
@@ -26,3 +27,5 @@ server.listen(8080,()=>{
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on('error',(error:Error)=>  console.log(error))
+
+app.use('/',router());

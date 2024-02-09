2986f1e642cd40ad170dba6475b3d510df60f34d
Ina Yoon <yia0319@gmail.com>
2024-02-09 10:07:40 -0500

    mongodb setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors({
@@ -19,3 +22,7 @@ const server = http.createServer(app);
 server.listen(8080,()=>{
   console.log('Server running on http://localhost:8080/')
 })
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on('error',(error:Error)=>  console.log(error))
